2827b9fa7e6a2bc91eef0ebb6f6139c76f37f0db
Jef <incutonez@hotmail.com>
2025-06-04 09:40:54 -0400

    - Renaming auth.storage.service.ts to session.storage.service.ts - Updating main.vuejobs.js

--- browserPlugin/main.vuejobs.js
+++ browserPlugin/main.vuejobs.js
@@ -1,5 +1,5 @@
 const ParentCard = ".transition-all";
-const LastCompany = "Bulba";
+const LastCompany = "Slides With Friends";
 let found = false;
 
 function docReady(fn) {
---
+++ packages/api/src/auth/session.storage.service.ts
@@ -0,0 +1,25 @@
+﻿import { Injectable } from "@nestjs/common";
+import { JWTPayload } from "express-oauth2-jwt-bearer";
+import { ClsService } from "nestjs-cls";
+import { ISessionStorage } from "@/types";
+
+@Injectable()
+export class SessionStorageService {
+	constructor(private readonly cls: ClsService<ISessionStorage>) {}
+
+	setMeasurementSystem(language: string) {
+		this.cls.set("measurementSystem", language.startsWith("en-US") ? "imperial" : "metric");
+	}
+
+	getMeasurementSystem() {
+		return this.cls.get("measurementSystem");
+	}
+
+	setUser(payload: JWTPayload) {
+		this.cls.set("user", payload);
+	}
+
+	getUserId() {
+		return this.cls.get("user").sub!;
+	}
+}

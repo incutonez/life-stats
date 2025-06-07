53e8260064e46be028badde7b885fffd780b171c
Jef <incutonez@hotmail.com>
2025-06-06 20:08:37 -0400

    - Removing strava spec, as we'll only need 1 endpoint from it, so it's a bit overkill - Figuring out how to get the user's access_token

--- packages/ui/src/api.ts
+++ packages/ui/src/api.ts
@@ -6,27 +6,12 @@
 	ExercisesApi,
 	JobsApi,
 } from "@incutonez/life-stats-spec";
-import { ActivitiesApi as ActivitiesStravaApi, Configuration as ConfigurationStrava } from "@incutonez/life-stats-spec-strava";
 
 export const apiConfig = new Configuration({
 	basePath: import.meta.env.VITE_BASE_API || `http://${location.hostname}:3000`,
 	baseOptions: {
 		headers: {},
 	},
-});
-
-/**
- * Source: https://developers.strava.com/docs/authentication/
- * TODOJEF: Need to use GET https://www.strava.com/oauth/authorize
- * Pass in
- * - client_id
- * - redirect_uri
- * - scope: read
- * - response_type: code
- */
-export const ApiStravaConfig = new ConfigurationStrava({
-	apiKey: import.meta.env.VITE_STRAVA_CLIENT_SECRET,
-	accessToken: import.meta.env.VITE_STRAVA_ACCESS_TOKEN,
 });
 
 export const JobsAPI = new JobsApi(apiConfig);
@@ -38,5 +23,3 @@ export const ApplicationsAPI = new ApplicationsApi(apiConfig);
 export const CompaniesAPI = new CompaniesApi(apiConfig);
 
 export const AuditsAPI = new AuditsApi(apiConfig);
-
-export const StravaActivitiesAPI = new ActivitiesStravaApi(ApiStravaConfig);
--- packages/ui/src/views/exercises/NavigationExercises.vue
+++ packages/ui/src/views/exercises/NavigationExercises.vue
@@ -1,6 +1,6 @@
 ﻿<script setup lang="ts">
 import { ref } from "vue";
-import { StravaActivitiesAPI } from "@/api.ts";
+import axios from "axios";
 import BaseButton from "@/components/BaseButton.vue";
 import { IconActivities, IconDownload, IconHistory } from "@/components/Icons.ts";
 import { useExerciseRoutes } from "@/views/exercises/composables/routes.ts";
@@ -9,6 +9,8 @@ import ViewActivitiesImport from "@/views/exercises/ViewActivitiesImport.vue";
 
 const showActivitiesImport = ref(false);
 const { isRouteSelected, viewHistory, ViewActivities } = useExerciseRoutes();
+const urlParams = new URLSearchParams(location.hash);
+const code = urlParams.get("code");
 
 function onClickViewActivities() {
 	ViewActivities();
@@ -22,10 +24,36 @@ function onClickImport() {
 	showActivitiesImport.value = true;
 }
 
+/**
+ * Our hands are tied with Strava... we MUST redirect the user to their authorize endpoint, which gives us back a code
+ * in the query string (if the user authorized our app).  We can then use the code to log into Strava with their token
+ * endpoint, which returns an access_token and a refresh_token.  The access_token expires 6 hours after it's issued...
+ * so before every request, check if expires_at < Date.now()... if so, use the logIn function below, but instead of
+ * using grant_type authorization_code, use refresh_token and pass refresh_token instead of code... when that comes back
+ * store the value like normal (including updated refresh_token and expires_at) and continue from there.
+ */
 async function onClickImportStrava() {
-	const response = await StravaActivitiesAPI.getLoggedInAthleteActivities();
-	console.info(response);
+	window.location.href = `https://www.strava.com/oauth/authorize?client_id=${import.meta.env.VITE_STRAVA_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(location.href)}&scope=read`;
 }
+
+async function logIn() {
+	if (code) {
+		// TODOJEF: We'll want to store the access_token, refresh_token, and expires_at in localStorage probably
+		// TODOJEF: I think this should be handled in the API because we don't want to expose our secret in the browser
+		await axios({
+			url: "https://www.strava.com/api/v3/oauth/token",
+			method: "POST",
+			params: {
+				code,
+				client_id: import.meta.env.VITE_STRAVA_CLIENT_ID,
+				client_secret: import.meta.env.VITE_STRAVA_CLIENT_SECRET,
+				grant_type: "authorization_code",
+			},
+		});
+	}
+}
+
+logIn();
 </script>
 
 <template>

7389cb2f2d13a8e0d2831fb7269e22cc373fd926
Jef <incutonez@hotmail.com>
2025-06-18 12:51:30 -0400

    feat: Adding UI Deployment to GH Pages (#68)

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,30 +2,34 @@ import tailwindcss from "@tailwindcss/vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { defineConfig } from "vite";
 import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
-export default {
-	// svgLoader is needed for material symbols
-	plugins: [vue(), svgLoader(), tailwindcss(), basicSsl({
-		name: "Life Stats",
-	})],
-	build: {
-		target: "esnext",
-	},
-	server: {
-		host: true,
-	},
-	resolve: {
-		alias: [{
-			// Add ability to use @ to represent the root dir being src
-			find: "@",
-			replacement: path.resolve(path.resolve(), "./src"),
-		}, {
-			/* This is necessary for mime-type
-			 * Source: https://github.com/jshttp/mime-types/issues/124 */
-			find: "path",
-			replacement: "path-browserify",
-		}],
-	},
-};
+export default defineConfig(({ mode }) => {
+	return {
+		base: mode === "GitHubPages" ? "/life-stats/" : "",
+		// svgLoader is needed for material symbols
+		plugins: [vue(), svgLoader(), tailwindcss(), basicSsl({
+			name: "Life Stats",
+		})],
+		build: {
+			target: "esnext",
+		},
+		server: {
+			host: true,
+		},
+		resolve: {
+			alias: [{
+				// Add ability to use @ to represent the root dir being src
+				find: "@",
+				replacement: path.resolve(path.resolve(), "./src"),
+			}, {
+				/* This is necessary for mime-type
+				 * Source: https://github.com/jshttp/mime-types/issues/124 */
+				find: "path",
+				replacement: "path-browserify",
+			}],
+		},
+	};
+});

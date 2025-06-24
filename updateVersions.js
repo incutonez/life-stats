7cd9b6da48f5fd701f577757726d5e207d8dc0cb
Jef <incutonez@hotmail.com>
2025-06-24 10:47:10 -0400

    fix: Testing

--- updateVersions.js
+++ updateVersions.js
@@ -4,10 +4,10 @@ const versionIndex = process.argv.indexOf("--version");
 const version = versionIndex >= 0 ? process.argv[versionIndex + 1] : undefined;
 if (version) {
 	// Updates packages/*
-	console.log(version);
+	console.log("HERE WE ARE", version);
 }
 else {
-	execSync("npx semantic-release --deps.bump=inherit", {
-		stdio: [0, 1, 2],
-	});
+	// execSync("npx semantic-release --deps.bump=inherit", {
+	// 	stdio: [0, 1, 2],
+	// });
 }

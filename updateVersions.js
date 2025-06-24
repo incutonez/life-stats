02cb128055d6e6f8e5c5e159a36dfa3a830a3b8a
Jef <incutonez@hotmail.com>
2025-06-24 09:50:31 -0400

    fix: Testing Version Updates (#79)

--- updateVersions.js
+++ updateVersions.js
@@ -1,12 +1,9 @@
 import { execSync } from "child_process";
-import { readdirSync } from "fs";
 
-const stdio = [0, 1, 2];
-readdirSync("packages/").forEach((packageName) => {
-	execSync("npx semantic-release --deps.bump=inherit", {
-		stdio,
-		cwd: `packages/${packageName}`,
-	});
-}, {
-	stdio,
+// Updates packages/*
+execSync("npx semantic-release --dry-run --no-ci", {
+	stdio: [0, 1, 2],
+});
+execSync("npx semantic-release --deps.bump=inherit", {
+	stdio: [0, 1, 2],
 });

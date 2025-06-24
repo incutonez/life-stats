75a602040f00316f42035d5a9c8f57694cd35ca9
Jef <incutonez@hotmail.com>
2025-06-24 11:28:29 -0400

    fix: testing

--- updateVersions.js
+++ updateVersions.js
@@ -1,9 +1,6 @@
 import { execSync } from "child_process";
 
 const stdio = [0, 1, 2];
-execSync("npx semantic-release --dry-run --no-ci", {
-	stdio
-});
 console.log(process.env)
 execSync(`echo HERE $NEXT_RELEASE_VERSION`, {
 	stdio

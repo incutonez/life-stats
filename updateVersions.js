e0bffff863447e7ec650584a31fd61a23baf4818
Jef <incutonez@hotmail.com>
2025-06-24 11:04:48 -0400

    fix: Testing

--- updateVersions.js
+++ updateVersions.js
@@ -1,13 +1,12 @@
 import { execSync } from "child_process";
 
-const versionIndex = process.argv.indexOf("--version");
-const version = versionIndex >= 0 ? process.argv[versionIndex + 1] : undefined;
-if (version) {
-	// Updates packages/*
-	console.log("HERE WE ARE", version);
-}
-else {
-	// execSync("npx semantic-release --deps.bump=inherit", {
-	// 	stdio: [0, 1, 2],
-	// });
-}
+const stdio = [0, 1, 2];
+execSync("npx semantic-release --dry-run --no-ci", {
+	stdio
+});
+execSync("echo $NEXT_RELEASE_VERSION", {
+	stdio
+});
+// execSync("npx semantic-release --deps.bump=inherit", {
+// 	stdio: [0, 1, 2],
+// });

409a3ebfd8f157621dd0cfe3c1ba072379c28a88
Jef <incutonez@hotmail.com>
2025-06-24 11:21:55 -0400

    fix: testing

--- updateVersions.js
+++ updateVersions.js
@@ -4,7 +4,8 @@ const stdio = [0, 1, 2];
 execSync("npx semantic-release --dry-run --no-ci", {
 	stdio
 });
-execSync(`echo HERE $NEXT_RELEASE_VERSION ${JSON.stringify(process.env)}`, {
+console.log(process.env)
+execSync(`echo HERE $NEXT_RELEASE_VERSION`, {
 	stdio
 });
 // execSync("npx semantic-release --deps.bump=inherit", {

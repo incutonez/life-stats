1749472adef642dff5b4f24005c931ae5b6e5fb5
Jef <incutonez@hotmail.com>
2025-06-24 11:34:11 -0400

    fix: Hopefully Updating Package Versions

--- updateVersions.js
+++ updateVersions.js
@@ -1,10 +1,19 @@
 import { execSync } from "child_process";
+import {existsSync, readdirSync} from "fs";
+import {readFileSync} from "node:fs";
 
 const stdio = [0, 1, 2];
-console.log(process.env)
-execSync(`echo HERE $NEXT_RELEASE_VERSION`, {
-	stdio
+const nextReleaseVersion = process.env.NEXT_RELEASE_VERSION;
+if (nextReleaseVersion) {
+	if (existsSync("packages/")) {
+		readdirSync("packages/").forEach((packageName) => {
+			const packageJSON = JSON.parse(readFileSync(`packages/${packageName}/package.json`, "utf8"));
+			packageJSON.version = nextReleaseVersion;
+		}, {
+			stdio,
+		});
+	}
+}
+execSync("npx semantic-release --deps.bump=inherit", {
+	stdio: [0, 1, 2],
 });
-// execSync("npx semantic-release --deps.bump=inherit", {
-// 	stdio: [0, 1, 2],
-// });

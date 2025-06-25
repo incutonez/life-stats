b533755bca2f04a886c59a89b9774deb3313b529
Jef <incutonez@hotmail.com>
2025-06-24 20:44:17 -0400

    feat: Adding GA and Persisting DB in Test (#87)
    * feat: Adding GA and Persisting DB in Test
    
    * - Transforming index.test.html to index.html for test builds

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,14 +1,24 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
+import { readFileSync } from "fs";
 import path from "path";
 import { defineConfig } from "vite";
 import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
 	return {
 		// svgLoader is needed for material symbols
-		plugins: [vue(), svgLoader(), tailwindcss()],
+		plugins: [vue(), svgLoader(), tailwindcss(), {
+			// Taken from https://github.com/vitejs/vite/discussions/3456#discussioncomment-750002
+			name: "transform-test-html",
+			apply: "build",
+			async transformIndexHtml() {
+				if (mode === "test") {
+					return readFileSync("./index.test.html", "utf-8");
+				}
+			},
+		}],
 		build: {
 			target: "esnext",
 		},
